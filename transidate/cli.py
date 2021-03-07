f30aa81e1e553db676cac70c95420c7a73c02375
Ciaran McCormick <ciaranmccormick@gmail.com>
2021-03-07 10:17:08 +0000

    Added ability to output violations as csv (#6)
    Co-authored-by: Ciaran McCormick <ciaran@ciaranmccormick.com>

--- transidate/cli.py
+++ transidate/cli.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import click
 from transidate.console import console
 from transidate.datasets import DataSet
+from transidate.outputs import ConsoleOutput, CSVOutput
 from transidate.validators import ValidationResult, Validators
 
 _SCHEMA_TYPES = Validators.registered_schemas
@@ -21,16 +22,22 @@ def cli():
     type=click.Choice(_SCHEMA_TYPES, case_sensitive=False),
     default="TXC2.4",
 )
-def validate(source: str, version: str):
+@click.option(
+    "--csv/--no-csv", "output_csv", help="Write violations to csv.", default=False
+)
+def validate(source: str, version: str, output_csv: bool):
     """
     Validate a transit data file against a specified schema.
     """
     header = "[bold red]"
+    breakpoint()
+
     console.rule(header + "Downloading Schema")
     schema = Validators.get_validator(version)
 
     console.rule(header + "Validating Documents")
-    dataset = DataSet(Path(source))
+    dataset_path = Path(source)
+    dataset = DataSet(dataset_path)
 
     result = schema.validate(dataset)
     ok = result.status == ValidationResult.OK
@@ -40,5 +47,6 @@ def validate(source: str, version: str):
         console.print("No issues found.")
     else:
         console.rule(header + f"Results: {len(result.violations)} Issues found")
-        for violation in result.violations:
-            console.print(f"{violation.filename}:{violation.line}: {violation.message}")
+        ConsoleOutput(dataset=dataset, result=result).output()
+        if output_csv:
+            CSVOutput(dataset=dataset, result=result).output()
